e1d5700e8acf19160e4c466c8ba7c8271ef8534a
choiyh0617 <6l0mrc00057@gmail.com>
2026-02-02 20:16:38 +0900

    fix: error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Header from './components/header/Header.tsx';
 import Home from './pages/home/Home.tsx';
-import AboutUs from './pages/AboutUs.tsx';
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <Home /> */}
-      <AboutUs />
+      <Home />
     </>
   )
 }
