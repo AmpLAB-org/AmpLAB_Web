74928acaf949c30542d4690c54d9db644b3cc409
cranis <cranis2@naver.com>
2026-02-03 15:41:51 +0900

    커뮤니티 페이지 하위 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
 import Header from "./components/header/Header";
 import About from "./pages/AboutUs";
 import Home from "./pages/Home";
 import Community from "./pages/Community";
+import Blog from "./pages/community/Blog";
+import News from "./pages/community/News";
+import CommunityBoard from "./pages/community/CommunityBoard";
 
 function App() {
   return (
@@ -13,7 +16,12 @@ function App() {
         <Route path="/about" element={<About />} />
         {/* <Route path="/download" element={<Download />} /> */}
         {/* <Route path="/contact" element={<Contact />} /> */}
-        <Route path="/community" element={<Community />} />
+        <Route path="/community" element={<Community />}>
+          <Route index element={<Navigate to="/community/blog" replace />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="news" element={<News />} />
+          <Route path="community" element={<CommunityBoard />} />
+        </Route>
       </Routes>
     </Router>
   );
