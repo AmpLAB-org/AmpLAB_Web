dbda3453d6ad0072ba65dce4b7e28b60cca0cf07
choiyh0617 <6l0mrc00057@gmail.com>
2026-02-03 16:11:25 +0900

    feat: service intro section

---
+++ src/assets/card/index.ts
@@ -0,0 +1,9 @@
+import Card_1 from "./service 2.svg";
+import Card_2 from "./service 3-2.svg";
+import Card_3 from "./service 2-2.svg";
+
+export {
+    Card_1,
+    Card_2,
+    Card_3
+}
---
+++ src/data/Service.ts
@@ -0,0 +1,19 @@
+import * as C from "../assets/card/index";
+
+export const services = [
+  {
+    id: "factoryx",
+    name: "FactoryX",
+    img: C.Card_1,
+  },
+    {
+    id: "factoryx",
+    name: "Sphere",
+    img: C.Card_2,
+  },
+    {
+    id: "factoryx",
+    name: "myCMO",
+    img: C.Card_3,
+  },
+];
