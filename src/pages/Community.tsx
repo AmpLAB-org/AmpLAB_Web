6bf296334b6615d3b3b174ae07ea506230aeaf88
cranis <cranis2@naver.com>
2026-02-03 19:34:26 +0900

    커뮤니티 페이지 중첩 라우터 준비

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -4,8 +4,12 @@ const Community = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // 현재 경로에서 하위 경로만 추출 (예: /community/blog -> blog)
-  const currentSubPath = location.pathname.split("/community/")[1] ?? "blog";
+  // 현재 경로에서 /community/ 뒤의 첫 세그먼트만 추출 (예: /community/blog/write -> blog)
+  const afterCommunity = location.pathname.split("/community/")[1] ?? "blog";
+  const currentSubPath = afterCommunity.split("/")[0] || "blog";
+
+  // /community/blog/write 인 경우에는 커뮤니티 페이지 UI를 숨김
+  const isWritePage = location.pathname.startsWith("/community/blog/write");
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     e.stopPropagation();
@@ -15,12 +19,16 @@ const Community = () => {
 
   return (
     <div>
-      커뮤니티 페이지
-      <select value={currentSubPath} onChange={handleChange}>
-        <option value="blog">블로그</option>
-        <option value="news">회사소식</option>
-        <option value="community">커뮤니티</option>
-      </select>
+      {!isWritePage && (
+        <>
+          커뮤니티 페이지
+          <select value={currentSubPath} onChange={handleChange}>
+            <option value="blog">블로그</option>
+            <option value="news">회사소식</option>
+            <option value="community">커뮤니티</option>
+          </select>
+        </>
+      )}
       <Outlet />
     </div>
   );
