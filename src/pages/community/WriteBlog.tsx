08f42891912b35ba5f3e19fa4762a8f3d3aeb79b
cranis <cranis2@naver.com>
2026-02-04 12:36:48 +0900

    axios 패키지 설치

--- src/pages/community/WriteBlog.tsx
+++ src/pages/community/WriteBlog.tsx
@@ -1,11 +1,39 @@
-const WriteBlog = () => {
+// @ts-nocheck
+import axios from "axios";
+// axios import 해야함
+
+import { useState, useEffect } from "react";
+
+function WriteBlog() {
+  async function uploadBlogPost() {
+    const formDataForBlog = new FormData();
+
+    console.log(document.getElementById("uploadimageinblog"));
+    console.log(document.getElementById("uploadimageinblog").files);
+    console.log(document.getElementById("uploadimageinblog").files[0]);
+
+    formDataForBlog.append(
+      "nameformulter",
+      document.getElementById("uploadimageinblog").files[0]
+    );
+
+    await axios.post(
+      "http://localhost:5013/homepage/blog/upload",
+      formDataForBlog
+    );
+  }
+
   return (
     <div>
       블로그 글쓰기 페이지
       <input />
+      <input type="file" id="uploadimageinblog" name="nameformulter" />
       <textarea />
+      <button type="button" onClick={uploadBlogPost}>
+        완료
+      </button>
     </div>
   );
-};
+}
 
 export default WriteBlog;
