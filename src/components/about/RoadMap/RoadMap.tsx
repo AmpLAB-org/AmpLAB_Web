56b088b848d197adcbb167fe573d5d83a8a12902
choiyh0617 <6l0mrc00057@gmail.com>
2026-02-07 03:55:17 +0900

    feat: roadmap card add animation

--- src/components/about/RoadMap/RoadMap.tsx
+++ src/components/about/RoadMap/RoadMap.tsx
@@ -1,6 +1,5 @@
 import * as S from "./RoadMap.styles";
 import bg from "../../../assets/background/bg_3.svg";
-import * as L from "../../../styles/common.styles";
 import { motion } from "framer-motion";
 import {
   BookCheck,
@@ -13,9 +12,10 @@ import {
 
 const RoadMap = () => {
   return (
-    <S.Content>
-      <L.BgImage src={bg} />
-      <L.Overlay>
+    <S.Section>
+      <S.BgImage src={bg} />
+
+      <S.Content>
         <S.Header>
           <S.SubTitle>How We Work</S.SubTitle>
           <S.Title>앰플랩의 핵심가치</S.Title>
@@ -27,26 +27,28 @@ const RoadMap = () => {
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 1 }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
           >
             <S.Item $side="left">
               <S.Dot>
                 <Zap />
               </S.Dot>
+
               <S.Card $side="left">
                 <S.Badge $side="left" $color="pink">Value 1</S.Badge>
                 <S.CardIcon $side="left" $color="pink">
                   <Handshake />
                 </S.CardIcon>
-                <div>
+
+                <S.CardContainer $side="right">
                   <S.CardTitle $color="pink">고객 성장</S.CardTitle>
-                  <S.CardDesc>
+                  <S.CardDesc $side="right">
                     고객의 비즈니스 성장을 돕고, 나아가
                     <br />
                     사회 전체가 성장할 수 있도록 기여합니다.
                   </S.CardDesc>
-                </div>
+                </S.CardContainer>
               </S.Card>
             </S.Item>
           </motion.div>
@@ -54,28 +56,30 @@ const RoadMap = () => {
           <motion.div
             initial={{ opacity: 0, y: 35 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 1.5 }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
           >
             <S.Item $side="right">
               <S.Dot>
                 <BookCheck />
               </S.Dot>
+
               <S.Card $side="right">
                 <S.Badge $side="right" $color="purple">Value 2</S.Badge>
                 <S.CardIcon $side="right" $color="purple">
                   <ArrowDownAZ />
                 </S.CardIcon>
-                <div>
+
+                <S.CardContainer $side="left">
                   <S.CardTitle $color="purple">끈기있는 실행</S.CardTitle>
-                  <S.CardDesc>
+                  <S.CardDesc $side="left">
                     망설이지 않고 시도하고 배우며 끝까지 해결합니다.
                     <br />
                     적극적으로 행동하고 무엇이든 끝까지
                     <br />
                     책임감 있게 실행합니다.
                   </S.CardDesc>
-                </div>
+                </S.CardContainer>
               </S.Card>
             </S.Item>
           </motion.div>
@@ -83,21 +87,23 @@ const RoadMap = () => {
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
           >
             <S.Item $side="left">
               <S.Dot>
                 <Globe />
               </S.Dot>
+
               <S.Card $side="left">
                 <S.Badge $side="left" $color="pink">Value 3</S.Badge>
                 <S.CardIcon $side="left" $color="pink">
                   <Share2 />
                 </S.CardIcon>
-                <div>
+
+                <S.CardContainer $side="right">
                   <S.CardTitle $color="pink">체계적인 공유</S.CardTitle>
-                  <S.CardDesc>
+                  <S.CardDesc $side="right">
                     우리는 정보공유보다 회고에서 배우는
                     <br />
                     지식을 공유하고 소통이 기본이라 믿습니다.
@@ -106,7 +112,7 @@ const RoadMap = () => {
                     <br />
                     공유하는것을 기본 원칙으로 삼습니다.
                   </S.CardDesc>
-                </div>
+                </S.CardContainer>
               </S.Card>
             </S.Item>
           </motion.div>
@@ -114,7 +120,7 @@ const RoadMap = () => {
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
           >
             <S.GoalWrap>
@@ -129,8 +135,8 @@ const RoadMap = () => {
             </S.GoalWrap>
           </motion.div>
         </S.Timeline>
-      </L.Overlay>
-    </S.Content>
+      </S.Content>
+    </S.Section>
   );
 };
 
