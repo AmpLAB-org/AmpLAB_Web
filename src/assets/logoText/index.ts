ba6646b343a2530ce4287ed3641acb489509cd4b
choiyh0617 <6l0mrc00057@gmail.com>
2026-02-12 18:52:15 +0900

    feat: svg animation

---
+++ src/assets/logoText/index.ts
@@ -0,0 +1,5 @@
+export { default as A } from "./A.svg?react";
+export { default as B } from "./B.svg?react";
+export { default as P } from "./P.svg?react";
+export { default as M } from "./M.svg?react";
+export { default as L } from "./L.svg?react";
