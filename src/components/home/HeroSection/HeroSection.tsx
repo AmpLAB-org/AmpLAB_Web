0bcfda8e5fc65219e672d8066f99869f369dd9ff
choiyh0617 <6l0mrc00057@gmail.com>
2026-02-13 11:42:16 +0900

    fix: scroll position

--- src/components/home/HeroSection/HeroSection.tsx
+++ src/components/home/HeroSection/HeroSection.tsx
@@ -7,19 +7,20 @@ import ScrollIndicator from "../../ScrollIndicator/ScrollIndicator";
 const HeroSection = () => {
   return (
     <L.Container>
-      <L.BgImage src={bg1} />
-      <L.Overlay>
-        <L.Content>
-          <S.Title>AI와 기술로</S.Title>
-          <S.Title>더 나은 확장을 연구하는 파트너,</S.Title>
-          <S.Subtitle>AmpLAB</S.Subtitle>
-          <S.Description>
-            가치를 확장하고, 비즈니스 성공을 함께 만들어갑니다.
-          </S.Description>
-        </L.Content>
-        <img src={logo} alt="logo" />
-        <ScrollIndicator />
-      </L.Overlay>
+      <L.Bg img={bg1}>
+        <L.Overlay>
+          <L.Content>
+            <S.Title>AI와 기술로</S.Title>
+            <S.Title>더 나은 확장을 연구하는 파트너,</S.Title>
+            <S.Subtitle>AmpLAB</S.Subtitle>
+            <S.Description>
+              가치를 확장하고, 비즈니스 성공을 함께 만들어갑니다.
+            </S.Description>
+            <img src={logo} alt="logo" />
+            <ScrollIndicator />
+          </L.Content>
+        </L.Overlay>
+      </L.Bg>
     </L.Container>
   );
 };
